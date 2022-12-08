101093fe8d2ec136979a3901d342ec2e5f4fcf29
Gilberto José Vento Alvarez <gilberto.vento@gmail.com>
2022-12-07 21:33:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 //1. imprime el texto hola mundo
-function printMessage() {
+function printMessage()
+{
 
 }
 
 //2. imprime la suma 2 y 3 
-function printSum2And3() {
+function printSum2And3()
+{
 
 }
 
 //3. imprime el producto 10 por 3 
-function printProduct() {
+function printProduct()
+{
 
 }
 
@@ -129,203 +132,243 @@ console.log(nums[i])
 */
 
 //4. intercambie dos numeros en un arreglo dados sus indices
-function swap(array, i, j) {
+function swap(array, i, j)
+{
 
 }
 
 //5. devuelve la suma de los dos numeros dados
-function sum(num1, num2) {
+function sum(num1, num2)
+{
 
 }
 
 //6. reste los dos numeros dados
-function minus(num1, num2) {
+function minus(num1, num2)
+{
 
 }
 
 //7. multiplique los dos numeros dados
-function mult(mult1, mult2) {
+function mult(mult1, mult2)
+{
 
 }
 
 //8. divida los dos numeros dados
-function divide(number, divisor) {
+function divide(number, divisor)
+{
 
 }
 
 //9. calcule el cuadrado del numero dado
-function square(num) {
+function square(num)
+{
 
 }
 
 //10. calcule el cubo del numero dado
-function cube(num) {
+function cube(num)
+{
 
 }
 
 //11. devuelva el resto de la division entera de dos numeros
-function reminder(number, divisor) {
+function reminder(number, divisor)
+{
 
 }
 
 //12. calcule el valor absoluto de un numero dado
-function abs(num) {
+function abs(num)
+{
 
 }
 
 //13. calcule el mayor numero de dos numeros
-function biggestTwo(num1, num2) {
+function biggestTwo(num1, num2)
+{
 
 }
 
 //14. calcule el mayor numero de tres numeros
-function biggestThree(num1, num2, num3) {
+function biggestThree(num1, num2, num3)
+{
 
 }
 
 //15. crea una funcion devuelva true si un numero es par y false si es inpar
-function isEven(num1) {
+function isEven(num1)
+{
 
 }
 
 //16.  realize la operacion dada 'suma', 'resta' , 'producto', 'division', 'resto', sobre dos numeros usando if/else if/else
-function calculateWithIf(operation, num1, num2) {
+function calculateWithIf(operation, num1, num2)
+{
 
 }
 
 //17.  realize la operacion dada 'suma', 'resta' , 'producto', 'division', 'resto', sobre dos numeros usando switch
-function calculateWithSwitch(operation, num1, num2) {
+function calculateWithSwitch(operation, num1, num2)
+{
 
 }
 
 //18. imprimir numeros del 1 al 5 sin usar ciclo for
-function printFrom1TO5() {
+function printFrom1TO5()
+{
 
 }
 
 //19. imprimir numeros del 1 al 1000 con un ciclo for
-function printFrom1TO1000() {
-
+function printFrom1TO1000()
+{
+    
 }
 
 //20. imprimir numeros pares del 1 al 1000 con un ciclo for
-function printEventNumbers1To1000() {
+function printEventNumbers1To1000()
+{
 
 }
 
 //21. imprimir numeros pares del 0 al 900 con un ciclo for
-function printEventNumbers0To900() {
+function printEventNumbers0To900()
+{
 
 }
 
 //22. sumar numeros del 1 al 2000 con un ciclo for
-function sumNumbers1To2000() {
+function sumNumbers1To2000()
+{
 
 }
 
 //23. sumar numeros inpares del 1 al 2000 con un ciclo for
-function sumOddNumbers1To2000() {
+function sumOddNumbers1To2000()
+{
 
 }
 
 //24. calcular promedio de los numeros inpares del 1000 al 3000 con un ciclo for
-function averageOdd1000To3000() {
+function averageOdd1000To3000()
+{
 
 }
 
 //25. imprimir numeros del 1 al 1000 con un ciclo while
-function printWithWhile() {
+function printWithWhile()
+{
 
 }
 
 //26. imprimir numeros pares del 1 al 1000 con un ciclo while
-function printEvenWithWhile1To1000() {
+function printEvenWithWhile1To1000()
+{
 
 }
 
 //27. imprimir numeros inpares del 0 al 999 con un ciclo while
-function printOddWithWhile0To999() {
+function printOddWithWhile0To999()
+{
 
 }
 
 //28. sumar numeros del 1000 al 2000 con un ciclo while
-function sumWithWhile1000To2000() {
+function sumWithWhile1000To2000()
+{
 
 }
 
 
 //29. sumar numeros impares del 1000 al 2000 con un ciclo while
-function sumOddsWithWhile1000To2000() {
+function sumOddsWithWhile1000To2000()
+{
 
 }
 
 //30. calcular promedio de los numeros del 1000 al 3000 con un ciclo while
-function averageWithWhile1000To3000() {
+function averageWithWhile1000To3000()
+{
 
 }
 
 //31.  sume todos los numeros del arreglo
-function sumArray(numbers) {
+function sumArray(numbers)
+{
 
 }
 
 //32.  calcule el mayor numero del arreglo
-function biggestInArray(numbers) {
+function biggestInArray(numbers)
+{
 
 }
 
 //33.  calcule el menor numero del arreglo
-function smallestInArray(numbers) {
+function smallestInArray(numbers)
+{
 
 }
 
 //34.  multiplique cada elemento del arreglo por 2
-function duplicateArray(numbers) {
+function duplicateArray(numbers)
+{
 
 }
 
 //35.  multiplique cada elemento del arreglo por si mismo
-function squareArray(numbers) {
+function squareArray(numbers)
+{
 
 }
 
 //36.  devuelva un arreglo de booleans indicando si cada elemento es par o inpar
-function parityArray(numbers) {
+function parityArray(numbers)
+{
 
 }
 
 //37.  calcule el promedio del arreglo
-function averageArray(numbers) {
+function averageArray(numbers)
+{
 
 }
 
 //38.  imprima los numeros pares en el arreglo
-function printEvenArray(numbers) {
+function printEvenArray(numbers)
+{
 
 }
 
 //39.  imprima los numeros inpares en el arreglo
-function printOddArray(numbers) {
+function printOddArray(numbers)
+{
 
 }
 
 //40.  cuente los numeros pares
-function countEvenArray(numbers) {
+function countEvenArray(numbers)
+{
 
 }
 
 //41.  cuente los numeros inpares
-function countOddArray(numbers) {
+function countOddArray(numbers)
+{
 
 }
 
 //42.  calcule el promedio de los numeros pares en el arreglo
-function averageEvenArray(numbers) {
+function averageEvenArray(numbers)
+{
 
 }
 
 //43.  calcule el promedio de los numeros inpares en el arreglo
-function averageOddArray(array) {
+function averageOddArray(array)
+{
 
 }
 
@@ -337,42 +380,50 @@ var products = [
     { name: "canela", price: 1 }
 ]
 */
-function printNames(products) {
+function printNames(products)
+{
 
 }
 
 //45. imprimir precios de productos
-function printPrices(products) {
+function printPrices(products)
+{
 
 }
 
 //46. aumentar precios de productos por la cantidad dada
-function increasePrice(products, amount) {
+function increasePrice(products, amount)
+{
 
 }
 
 //47. crear un arreglo con los nombres de los productos
-function getNames(products) {
+function getNames(products)
+{
 
 }
 
 //48. crear un arreglo con los precios de los productos
-function getPrices(products) {
+function getPrices(products)
+{
 
 }
 
 //49. devuelve el producto con el mayor precio
-function mostExpensiveProduct(products) {
+function mostExpensiveProduct(products)
+{
 
 }
 
 //50. devuelve el producto con el menor precio
-function cheapestProduct(products) {
+function cheapestProduct(products)
+{
 
 }
 
 //51. buscar y devuelver un producto dado su nombre
-function getProductByName(products, productName) {
+function getProductByName(products, productName)
+{
 
 }
 
